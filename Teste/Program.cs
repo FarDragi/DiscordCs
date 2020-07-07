df58819944788a3c1c0e93016402cb9fe9077974
FarDragi <mrvulcandragonex@gmail.com>
2020-07-07 16:20:54 -0300

    Projeto de teste da api

---
+++ Teste/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FarDragi.DragiCordApi.Teste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
