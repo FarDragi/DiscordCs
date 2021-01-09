77b5c3d66abd2f60ad6ce0c71080a281cc055ca2
Gabriel-Paulucci <gabrielpauluccimunizmendes@gmail.com>
2021-01-09 16:28:04 -0300

    Base do gateway

---
+++ src/FarDragi.DiscordCs.Gateway/Models/Payload/BasePayload.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FarDragi.DiscordCs.Gateway.Models.Payload
+{
+    public class BasePayload<T>
+    {
+        public int OpCode { get; set; }
+        public T Data { get; set; }
+        public int SequenceNumber { get; set; }
+        public string Event { get; set; }
+    }
+}
