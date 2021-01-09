a45e84acdbb6cb5b871e1c14fb0b19bc1bc5d73e
Gabriel-Paulucci <gabrielpauluccimunizmendes@gmail.com>
2021-01-09 16:17:05 -0300

    Adicionei o link do resume

--- src/FarDragi.DiscordCs.Core/Resume/IDiscordResume.cs
+++ src/FarDragi.DiscordCs.Core/Resume/IDiscordResume.cs
@@ -4,6 +4,9 @@ using System.Text;
 
 namespace FarDragi.DiscordCs.Core.Resume
 {
+    /// <summary>
+    /// https://discord.com/developers/docs/topics/gateway#resume-example-resume
+    /// </summary>
     public interface IDiscordResume
     {
         public string Token { get; set; }
