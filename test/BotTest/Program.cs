dd90a2b83bff71ca742f6e3a794920bf41bd44bf
Gabriel-Paulucci <gabrielpauluccimunizmendes@gmail.com>
2021-01-17 00:08:17 -0300

    Cansei de toda hora ter q tirar o token kkk

--- test/BotTest/Program.cs
+++ test/BotTest/Program.cs
@@ -1,5 +1,7 @@
 ﻿using FarDragi.DiscordCs;
 using System;
+using System.Collections.Specialized;
+using System.Configuration;
 using System.Threading.Tasks;
 
 namespace BotTest
@@ -8,9 +10,21 @@ namespace BotTest
     {
         static async Task Main(string[] args)
         {
+            string token = ConfigurationManager.AppSettings["token"];
+            if (token == null)
+            {
+                Configuration configFile = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+                KeyValueConfigurationCollection settings = configFile.AppSettings.Settings;
+                Console.Write("Token: ");
+                token = Console.ReadLine();
+                settings.Add("token", token);
+                configFile.Save(ConfigurationSaveMode.Modified);
+                ConfigurationManager.RefreshSection(configFile.AppSettings.SectionInformation.Name);
+            }
+
             Client client = new(new ClientConfig
             {
-                Token = ""
+                Token = token
             });
 
             client.Login();
