2b3c00bc0b9efec3f3d5d9edca9986ca5fe6f501
Gabriel-Paulucci <gabrielpauluccimunizmendes@gmail.com>
2021-01-19 20:22:45 -0300

    Base das listas

---
+++ src/FarDragi.DiscordCs/Collections/CollectionBase.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FarDragi.DiscordCs.Collections
+{
+    public class CollectionBase<TType>
+    {
+        private readonly Dictionary<string, TType> pairs;
+
+        public CollectionBase()
+        {
+            pairs = new Dictionary<string, TType>();
+        }
+
+        public TType this[string id]
+        {
+            get
+            {
+                return pairs[id];
+            }
+            set
+            {
+                pairs[id] = value;
+            }
+        }
+    }
+}
