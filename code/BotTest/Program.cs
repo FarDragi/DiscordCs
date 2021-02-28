24c34b6ac8e88c7d5f65b7bd297371c4c755fc59
Gabriel-Paulucci <gabrielpauluccimunizmendes@gmail.com>
2021-02-28 18:06:25 -0300

    Reestruturando o projeto

---
+++ code/BotTest/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Configuration;
+
+namespace BotTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string token = ConfigurationManager.AppSettings["token"];
+            if (token == null)
+            {
+                Configuration configFile = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+                KeyValueConfigurationCollection settings = configFile.AppSettings.Settings;
+                Console.Write("Token: ");
+                token = Console.ReadLine();
+                settings.Add("token", token);
+                configFile.Save(ConfigurationSaveMode.Modified);
+                ConfigurationManager.RefreshSection(configFile.AppSettings.SectionInformation.Name);
+            }
+        }
+    }
+}
